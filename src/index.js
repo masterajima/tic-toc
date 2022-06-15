b8a7ab33e99f794169a00dd0213eaa035f985bd9
Ajima Master <98704309+masterajima@users.noreply.github.com>
2022-06-14 22:42:59 -0400

    Pass value to square

--- src/index.js
+++ src/index.js
@@ -22,5 +22,43 @@ class Board extends React.Component{
     renderSquare(i){
 return<square value={i}/>
     }
+render(){
+        const status='Next Player:X'
+        return(
+            <div>
+                <div className='status'>{letstatus}</div>
+                <div className='board-row'>
+                    {/* call square method */}
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className='board-row'>
+                    {/* call square method */}
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className='board-row'>
+                    {/* call square method */}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+
+            </div>
+        )
+    }
+
+}
+class square extends React.Component{
+    render(){
+        return(
+            <button className="square">{this.props.value}
+
+            </button>
+        )
+    }
+}
 
 }
